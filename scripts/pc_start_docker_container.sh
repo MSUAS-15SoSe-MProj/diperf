e11b12e3ca605e4583e66c2e6797b5c68599c43c
pi cluster <markus.boese@gmail.com>
2015-09-22 21:11:54 +0000

    hostname fix

--- scripts/pc_start_docker_container.sh
+++ scripts/pc_start_docker_container.sh
@@ -1,6 +1,10 @@
-#!/bin/bash
+#!/usr/bin/zsh
+cmd="docker run -d --privileged -p 22222:22 -p 40000-40010:40000-40010 --env MPIR_CVAR_CH3_PORT_RANGE=40000:40010 --hostname=HOSTNAME-docker moddus/fit:latest"
+
 for i in `seq 1 5`;
 do
-	ssh pi$i	"docker run -d --privileged -p 22222:22 -p 40000-40100:40000-40100 --env MPIR_CVAR_CH3_PORT_RANGE=40000:40100 --hostname=$HOSTNAME-docker-container moddus/fit:latest" &
+	hostname_to=pi$i
+	full_cmd="${cmd/HOSTNAME/$hostname_to}"
+	ssh pi$i $full_cmd &
 done
 wait
