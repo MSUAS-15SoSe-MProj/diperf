625442f0a7c87ece5cc8f249b8206f41ed1c3302
pi cluster <markus.boese@gmail.com>
2015-09-25 11:08:03 +0000

    Corrected pi-start-scriptx

--- scripts/pc_start_docker_container.sh
+++ scripts/pc_start_docker_container.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/zsh
-cmd="docker run -d --privileged -p 22222:22 -p 40000-40010:40000-40010 --env MPIR_CVAR_CH3_PORT_RANGE=40000:40010 --hostname=HOSTNAME-docker moddus/fit:latest"
+cmd="docker run -d --privileged -p 22222:22 -p 40000-40020:40000-40020 --env MPIR_CVAR_CH3_PORT_RANGE=40000:40020 --hostname=HOSTNAME-docker moddus/pi_cluster:latest"
 
 for i in `seq 1 5`;
 do
