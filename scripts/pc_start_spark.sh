7422f58380262d93e823074da5ce30f5321cdf00
Pi Cluster <markus.boese@gmail.com>
2015-10-24 15:31:23 +0000

    Created script to start spark-cluster

---
+++ scripts/pc_start_spark.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+compose_master="$(cat $1)"
+compose_slave="$(cat $2)"
+
+# Start spark master
+full_cmd_master="${compose_master/HOSTNAME/$hostname_to}"
+ssh pi$i "echo '$full_cmd_master' | docker-compose -f - up -d" &
+
+for i in `seq 2 5`;
+do
+	hostname_to=pi$i
+	full_cmd_slave="${compose_slave/HOSTNAME/$hostname_to}"
+	ssh pi$i "echo '$full_cmd_slave' | docker-compose -f - up -d" &
+done
+wait
